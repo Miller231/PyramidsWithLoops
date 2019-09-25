f9359ba8787202b3d5d266bf47afc232a6eb489e
austi <austi@DESKTOP-TOVB19S>
2019-09-25 10:23:52 -0400

    Finished!

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -14,13 +14,15 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 1, j = 1; i <= lines; i++, j=j+2) {
-			for (int k = 1; k <= j; k++) {
+		int lines = 5; // Declaring how many lines of our pyramid we want
+		for (int i = 1, l = 5, j = 1; i <= lines; i++, l--, j=j+2) { // Loop to set up incrementing
+			for (int m = 1; m <= l; m++) { // inner loop to provide correct spacing
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= j; k++) { // inner loop to provide correct starting number
 				System.out.print(i);
 			}
-
-			System.out.println();
+			System.out.println(); // Prints space in between lines
 		}
 	}
 }
